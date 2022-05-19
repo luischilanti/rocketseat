92db2b0332209e82d74efab24787e5b7840b14e0
Luis Chilanti Brollo <50107509+luischilanti@users.noreply.github.com>
2022-05-19 20:35:20 -0300

    refactor: refactoring to typescript

--- 01-github-explorer/src/components/RepositoryList.tsx
+++ 01-github-explorer/src/components/RepositoryList.tsx
@@ -5,8 +5,15 @@ import "../styles/index.scss"
 
 // https://api.github.com/orgs/rocketseat/repos
 
+interface Repository {
+  name: string,
+  description: string,
+  html_url: string
+}
+
 export function RepositoryList () {
-  const [repositories, setRepositories] = useState([])
+  const [repositories, setRepositories] = useState<Repository[]>([])
+
   useEffect(() => {
     fetch("https://api.github.com/orgs/rocketseat/repos")
     .then( response => response.json())
