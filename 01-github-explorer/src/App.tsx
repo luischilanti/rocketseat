da0ca83e968fbe85570121685ce086561477136e
Luis Chilanti Brollo <50107509+luischilanti@users.noreply.github.com>
2022-06-22 13:00:15 -0300

    feat: git commit

--- 01-github-explorer/src/App.tsx
+++ 01-github-explorer/src/App.tsx
@@ -2,7 +2,9 @@ import "./styles/index.scss"
 import { RepositoryList } from './components/RepositoryList'
 
 export function App() {
-    return (
+    return (<>
         <RepositoryList/>
+        <h1>teste do luis</h1>
+        </>
     )
 }
