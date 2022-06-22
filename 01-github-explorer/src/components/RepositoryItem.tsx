548abf1f9f85493cf6965aac6f14c423050f327f
Luis Chilanti Brollo <50107509+luischilanti@users.noreply.github.com>
2022-06-22 12:45:05 -0300

    feat: retornando o codigo base

--- 01-github-explorer/src/components/RepositoryItem.tsx
+++ 01-github-explorer/src/components/RepositoryItem.tsx
@@ -9,7 +9,11 @@ interface RepositoryItemProps {
 export function RepositoryItem (props: RepositoryItemProps) {
     return (
         <li>
-            
+            <strong>{props.repository?.name}</strong>
+            <p>{props.repository.description}</p>
+            <a href={props.repository.html_url}>
+                Acessar repositório
+            </a>
         </li>
     )
 }
