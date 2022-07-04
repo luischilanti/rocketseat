a17b31a88ed61adaaa1ab7f2c8b84a875bff19cf
Luis Chilanti Brollo <50107509+luischilanti@users.noreply.github.com>
2022-07-04 02:06:47 -0300

    feat: fixing loaded data

--- 02-web-application/src/index.tsx
+++ 02-web-application/src/index.tsx
@@ -17,15 +17,15 @@ createServer({
           type: "deposit",
           category: "Dev",
           amount: 6000,
-          createdAt: new Date("2022-07-11 09:00:00"),
+          createdAt: new Date("2022-07-15 09:00:00"),
         },
         {
           id: 2,
           title: "Aluguel",
           type: "withdraw",
           category: "Casa",
-          amount: 1100,
-          createdAt: new Date("2022-07-20 16:30:00"),
+          amount: 2300,
+          createdAt: new Date("2022-07-14 16:30:00"),
         },
       ],
     });
