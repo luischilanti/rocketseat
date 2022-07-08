557f829df5c2ecb239b276b00da3e94323d7051a
Luis Chilanti Brollo <50107509+luischilanti@users.noreply.github.com>
2022-07-08 01:34:58 -0300

    feat: creating custom hook

--- 02-web-application/src/components/NewTransaction/index.tsx
+++ 02-web-application/src/components/NewTransaction/index.tsx
@@ -1,13 +1,12 @@
 import Modal from "react-modal";
-import { FormEvent, useContext, useState } from "react";
-import { api } from "../../services/api";
+import { FormEvent, useState } from "react";
 
 import { Container, TransactionType, RadioBox } from "./styles";
 
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
-import { TransactionsContext } from "../../TransactionsContext";
+import { useTransactions } from "../../hooks/useTransactions";
 
 interface NewTransactionProps {
   isOpen: boolean;
@@ -18,7 +17,7 @@ export function NewTransaction({
   isOpen,
   onRequestClose,
 }: NewTransactionProps) {
-  const { createTransaction } = useContext(TransactionsContext);
+  const { createTransaction } = useTransactions();
 
   const [title, setTitle] = useState("");
   const [amount, setAmount] = useState(0);
