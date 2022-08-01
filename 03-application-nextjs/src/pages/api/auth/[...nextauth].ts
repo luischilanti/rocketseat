9041bacff47e65349532b60aa6cff73c5ad5b49e
Luis Chilanti Brollo <50107509+luischilanti@users.noreply.github.com>
2022-08-01 14:19:26 -0300

    feat: adding callback to save a user

--- 03-application-nextjs/src/pages/api/auth/[...nextauth].ts
+++ 03-application-nextjs/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { fauna } from "../../../services/fauna";
+import { query as q } from "faunadb";
 
 export default NextAuth({
   providers: [
@@ -8,4 +10,17 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
