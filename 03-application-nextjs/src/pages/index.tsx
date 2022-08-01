1340c1c1a7df7a63ee2f99b993f91ad97e62878d
Luis Chilanti Brollo <50107509+luischilanti@users.noreply.github.com>
2022-07-31 23:17:39 -0300

    feat: addin static site generation ssg

--- 03-application-nextjs/src/pages/index.tsx
+++ 03-application-nextjs/src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import styles from "./home.module.scss";
 
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import { stripe } from "../services/stripe";
 
 interface HomeProps {
@@ -39,7 +39,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1LRnhnLFHMOaA8zfF1JvBYZ4");
 
   const product = {
@@ -54,5 +54,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
