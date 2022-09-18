6109c111281d41648f25aa9dc1619e0ec89b4fcc
Luis Chilanti Brollo <50107509+luischilanti@users.noreply.github.com>
2022-09-18 20:54:27 -0300

    feat: component ActiveLink

--- 03-application-nextjs/src/components/Header/index.tsx
+++ 03-application-nextjs/src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import { SignInButton } from "../SignInButton"
 import styles from "./styles.module.scss"
-import Link from "next/link"
+import { ActiveLink } from "../ActiveLink"
 
 export function Header() {
   return (
@@ -8,13 +8,13 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <Link href="/">
-            <a className={styles.active}>Home</a>
-          </Link>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
 
-          <Link href="/posts" prefetch>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
             <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
