dcadf5a1bad93800c5fbccacade03de01cc7b21d
Luis Chilanti Brollo <50107509+luischilanti@users.noreply.github.com>
2022-09-18 22:37:11 -0300

    feat: creating intern page post

--- 03-application-nextjs/src/pages/posts/index.tsx
+++ 03-application-nextjs/src/pages/posts/index.tsx
@@ -1,8 +1,10 @@
 import { GetStaticProps } from "next"
-import Head from "next/head"
 import { getPrismicClient } from "../../services/prismic"
-import styles from "./styles.module.scss"
+import Head from "next/head"
 import { RichText } from "prismic-dom"
+
+import styles from "./styles.module.scss"
+import Link from "next/link"
 
 type Post = {
   slug: string;
@@ -10,11 +12,11 @@ type Post = {
   excerpt: string;
   updatedAt: string;
 }
-interface PostsProsp {
-  posts: Post[]
+interface PostsProps {
+  posts: Post[];
 }
 
-export default function Posts({ posts }: PostsProsp) {
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -24,12 +26,14 @@ export default function Posts({ posts }: PostsProsp) {
       <main className={styles.container}>
         <div className={styles.posts}>
 
-          { posts.map(post => (
-            <a href="#" key={post.slug}>
-              <time>{post.updatedAt}</time>
-              <strong>{post.title} </strong>
-              <p>{post.excerpt}</p>
-            </a>
+          {posts.map(post => (
+            <Link href={`/posts/${post.slug}`}>
+              <a key={post.slug}>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title} </strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
 
         </div>
@@ -49,7 +53,7 @@ export const getStaticProps: GetStaticProps = async () => {
     return {
       slug: post.uid,
       title: RichText.asText(post.data.title),
-      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      excerpt: post.data.content.find((content: { type: any }) => content.type === 'paragraph')?.text ?? '',
       updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
         day: '2-digit',
         month: 'long',
